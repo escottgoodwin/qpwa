966a93e0dacf53591a380b865afd629100ba2e62
Evan Goodwin <escottgoodwin@gmail.com>
2019-06-12 14:39:49 -0700

    test questions buttons

--- src/components/TeacherQuestionButtons.js
+++ src/components/TeacherQuestionButtons.js
@@ -1,13 +1,16 @@
 import React,{Component} from 'react'
 import '../css/App.css'
 import { Link, withRouter } from 'react-router-dom'
-
+import moment from 'moment'
 import { useMutation } from 'react-apollo-hooks';
 
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import orange from '@material-ui/core/colors/orange';
 
 import { RELEASE_QUESTIONS_MUTATION, TEST_QUERY } from '../ApolloQueries'
 
@@ -42,27 +45,20 @@ const TeacherQuestionButtons = props =>
 
       <div style={{marginBottom:20}}>
 
-      {props.release ?
-        <>
-        <Card style={{marginTop:20}}>
-        <CardContent >
-        Release Date
-        </CardContent>
-        </Card>
-        </>
-        :
-
-        releaseQuestion(props.id)
-      }
-
       {
         props.published ?
 
         <>
         <Card style={{marginTop:20}}>
+        <CardContent style={{ backgroundColor:orange[100]}}>
+        <Typography style={{color:orange[800]}} variant="h5" component="h5">
+          Test Published
+        </Typography>
+        </CardContent>
         <CardContent >
-        <div>Publish Date</div>
-        <div>Time parameters</div>
+        <h5>Date: {moment(props.publishDate).format('MMMM Do, YYYY')}</h5>
+        <div><h5>Time Frame: {props.startTime} - {props.endTime}</h5></div>
+        <div><h5>End Date: {moment(props.endDate).format('MMMM Do YYYY, h:mm A')}</h5></div>
         </CardContent>
         </Card>
         </>
@@ -78,6 +74,26 @@ const TeacherQuestionButtons = props =>
         <Button  style={{marginTop:20}} fullWidth size='large' variant='contained' color="primary" >Publish Test</Button>
 
         </Link>
+      }
+
+      {props.release ?
+        <>
+        <Card style={{marginTop:20}}>
+        <CardContent style={{ backgroundColor:deepPurple[100]}}>
+        <Typography style={{color:deepPurple[800]}} variant="h5" component="h5">
+          Questions Released
+        </Typography>
+        </CardContent>
+
+        <CardContent >
+          <h5>Date: {moment(props.releaseDate).format('MMMM Do, YYYY')}</h5>
+          <h5>Total Questions: {props.questions.length}</h5>
+        </CardContent>
+        </Card>
+        </>
+        :
+
+        releaseQuestion(props.id)
       }
 
       </div>
